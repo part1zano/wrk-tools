5f92355e19bcda06faf14a6b79124510c9c8e001
Maxim V Filimonov <part1zancheg@gmail.com>
2013-01-04 05:04:15 +0400

    Added setup.py for teh buildz
    Subj
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
    
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   setup.py

---
+++ qjson/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='qjson',
+		version='0.1',
+		description='A small PyQt4 application for some json shit',
+		author='Maxim Kirenenko',
+		author_email='part1zancheg@gmail.com',
+		url='http://ya.ru',
+		py_modules=['qjcommon.myrandom', 'qjcommon.util'],
+		scripts=['qjson.py'],
+		data_files=[('share/qjson/imgs', ['imgs/button-cross.png', 'imgs/database.png', 'imgs/floppy-disk.png', 'imgs/folder.png', 'imgs/question.png', 'imgs/quit.png', 'imgs/search.png', 'imgs/toggle-collapse-alt.png', 'imgs/toggle-expand-alt.png']), ('share/qjson/ui', ['ui/qjson.ui'])]
+		)
+
